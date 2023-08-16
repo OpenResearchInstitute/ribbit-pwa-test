23576d6c64851f486c0b873bb65d60dca97f0629
Rich Clingman <richclingman@gmail.com>
2023-08-16 14:23:56 -0500

    Add GPS location & time to header (will want in message traffic)

--- app.vue
+++ app.vue
@@ -6,8 +6,29 @@
       <nuxt-link to="/">Home</nuxt-link>
       ...
       <nuxt-link to="/dsp/example3">Example 3</nuxt-link>
+      <div>
+        GPS: {{ latitude }} {{ longitude }} {{ timestamp }}
+      </div>
     </nav>
 
     <NuxtPage/>
   </div>
 </template>
+
+<script setup>
+const latitude = ref(0)
+const longitude = ref(0)
+const timestamp = ref(0)
+
+if ("geolocation" in navigator) {
+  console.log('got geo')
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position)
+    latitude.value = position.coords.latitude
+    longitude.value = position.coords.longitude
+    timestamp.value = new Date(position.timestamp).toUTCString()
+
+  })
+}
+
+</script>
